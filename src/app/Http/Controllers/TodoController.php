c1bff591d3b1ebebb4ecaca4716e64194ed9bc9e
st12516 <mrym1126eight@gmail.com>
2026-05-01 18:37:44 +0900

    refactor: コントローラのリファクタリング

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -7,10 +7,17 @@ use App\Todo;
 
 class TodoController extends Controller
 {
+    private $todo;
+
+    public function __construct(Todo $todo)
+    {
+        $this->todo = $todo;
+    }
+    
     public function index()
     {
-        $todo = new Todo();
-        $todos = $todo->all();
+        // $todo = new Todo();
+        $todos = $this->todo->all();
         // dd($todos);
 
         return view('todo.index', ['todos' => $todos]);
@@ -27,19 +34,19 @@ class TodoController extends Controller
         $inputs = $request->all();
         // dd($inputs);
         // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
-        $todo = new Todo();
+        // $todo = new Todo();
         // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入、連想配列
-        $todo->fill($inputs);
+        $this->todo->fill($inputs);
         // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
-        $todo->save();
+        $this->todo->save();
         return redirect()->route('todo.index');
     }
 
     public function show($id) 
     {
         // dd($id);
-        $model = new Todo();
-        $todo = $model->find($id);
+        // $model = new Todo();
+        $todo = $this->todo->find($id);
         // dd($todo);
         return view('todo.show', ['todo' => $todo]);
     }
