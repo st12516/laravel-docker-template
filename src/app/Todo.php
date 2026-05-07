e3e25e898b8aecbb874df178bf2e2a7077887ccf
st12516 <mrym1126eight@gmail.com>
2026-05-07 16:53:30 +0900

    feat: 論理削除機能追加

--- src/app/Todo.php
+++ src/app/Todo.php
@@ -3,11 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes; // 追記
 
 class Todo extends Model
 {
+    use SoftDeletes; // 追記
+
     protected $table = 'todos';
+
     protected $fillable = [
         'content',
     ];
 }
+
+// ToDoの削除時にdeleted_atカラムに削除日時を格納して更新
+// ToDoの取得時にdeleted_atカラムがNULLのレコードのみという条件を追加
