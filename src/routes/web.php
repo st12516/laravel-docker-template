cf0293c8dcaf4a324187f1e1c65c9e37d0572d24
st12516 <mrym1126eight@gmail.com>
2026-05-07 14:29:29 +0900

    feat: DBデータ更新機能追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -24,3 +24,6 @@ Route::get('/todo', 'TodoController@index')->name('todo.index');
 Route::get('/todo/{id}', 'TodoController@show')->name('todo.show');
 
 Route::get('/todo/{id}/edit', 'TodoController@edit')->name('todo.edit');
+
+// 更新ボタンをクリックした際にリクエストするルート、編集対象を1件特定するため、ルートパラメータで更新するToDoのIDを受け取れる
+Route::put('/todo/{id}', 'TodoController@update')->name('todo.update');
